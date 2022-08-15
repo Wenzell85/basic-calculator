fae6adad819371c205e8875eab1a696b40bf02d6
Wenzell85 <mbudane.m@gmail.com>
2022-08-15 22:06:45 +0200

    Create files and basic calculator functions

---
+++ script.js
@@ -0,0 +1,24 @@
+// Basic calculator functions 
+
+
+const add = function(a, b){
+    return a + b;
+};
+
+const subtract = function(a, b){
+    return a - b;
+}
+
+
+const multiply = function(a, b){
+    return a * b;
+}
+
+const divide = function(a, b){
+    return a / b;
+}
+
+
+
+
+console.log(divide(4, 2)); 
