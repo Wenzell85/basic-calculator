71d99bf63ac1ca9683934062518efda964808943
Wenzell85 <mbudane.m@gmail.com>
2022-08-18 00:54:53 +0200

    Create button event listeners display and equal function

--- script.js
+++ script.js
@@ -1,32 +1,97 @@
-// Basic calculator functions 
+
+const button = document.querySelectorAll('.butt'); //each button clicked 
+const display = document.querySelector('.display'); //Display 
+const equalButt = document.querySelector('#equal'); //equal button
+let expression = '';
 
 
-const add = function(a, b){
+
+button.forEach((item) => {
+    item.addEventListener('click', buttonClick);
+
+});
+
+
+
+//*************************************function library**************************************************/
+
+
+//button  click function 
+
+function buttonClick(e) {
+    expression += this.innerText;    
+    if (expression.length > 12) {
+
+        let maxDigits = expression.slice(0, 12);
+
+        display.innerText = maxDigits;
+        
+    }else{
+
+        display.innerText = expression;
+    } 
+    
+    
+    if(this.innerText == 'C'){
+    
+        display.innerText = '0';
+        expression = '';
+
+    }
+
+    if(expression.slice(0, 2) == '00'){
+
+        display.innerText = '0';
+        expression = '';
+    }
+
+    
+
+};
+
+//Equal button click
+
+equalButt.addEventListener('click', operate);
+
+
+
+// Basic calculator functions
+
+const add = function (a, b) {
     return a + b;
 };
 
-const subtract = function(a, b){
+const subtract = function (a, b) {
     return a - b;
-}
+};
 
 
-const multiply = function(a, b){
+const multiply = function (a, b) {
     return a * b;
-}
+};
 
-const divide = function(a, b){
+const divide = function (a, b) {
     return a / b;
+};
+
+
+//Operator function       
+
+
+function operate (e) {
+
+    let answer = eval(expression);
+
+    display.innerText = answer;     
+
+    expression = answer;
+
 }
 
 
-//Operator function 
 
-const operate = function(operator, a, b){
-
-    return operator(a, b);
-}
+//****************************************************************************************************
 
 
 
 
-//sconsole.log(divide(4, 2)); 
